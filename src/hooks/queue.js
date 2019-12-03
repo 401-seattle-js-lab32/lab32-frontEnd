51fd152e3a1403baa6caa2657c2efdbc0103a118
Corey Marchand <corey.marchand@me.com>
2019-12-03 09:38:22 -0800

    hooks complete

---
+++ src/hooks/queue.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const useQueue = (Q, queue) => {
+  const queueSubscribe = (event, callback) => {
+    queue.subscribe(event, payload => {
+      callback(payload);
+    });
+  };
+
+  const queuePublish = (q, event, message) => {
+    Q.publish(q, event, message)
+  };
+
+  return [queuePublish, queueSubscribe];
+}
+
+export default useQueue;
